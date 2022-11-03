3ceabd03e07c7eb02d55527b15c1d11476d0b46a
Dennis Wenrich <dennisfinley@gmail.com>
2022-11-03 19:21:58 -0400

    the login and registration components were added

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
